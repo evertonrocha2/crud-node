501e1b0abc4725c1020adc5db738a77c334cf194
evertonrocha2 <evertondarocha2018@gmail.com>
2024-07-21 15:34:01 -0300

    :sparkles: feat: get all implementation

--- src/services/CarroService.js
+++ src/services/CarroService.js
@@ -9,47 +9,4 @@ module.exports = {
       });
     });
   },
-  getById: (id) => {
-    return new Promise((resolve, reject) => {
-      db.query("SELECT * FROM carros WHERE codigo = ?", [id], (err, res) => {
-        if (err) reject(err);
-        resolve(res);
-      });
-    });
-  },
-  insertCar: (model, placa) => {
-    return new Promise((resolve, reject) => {
-      db.query(
-        "INSERT INTO carros (modelo, placa) VALUES (?, ?)",
-        [model, placa],
-        (err, res) => {
-          if (err) reject(err);
-          resolve(res.insertId);
-        }
-      );
-    });
-  },
-  updateCar: (code, model, placa) => {
-    return new Promise((resolve, reject) => {
-      db.query(
-        "UPDATE carros SET modelo = ?, placa = ? WHERE codigo = ?",
-        [model, placa, code],
-        (err, res) => {
-          if (err) {
-            reject(err);
-            return;
-          }
-          resolve(res.affectedRows);
-        }
-      );
-    });
-  },
-  deleteCar: (code) => {
-    return new Promise((resolve, reject) => {
-      db.query("DELETE FROM carros WHERE codigo = ?", [code], (err, res) => {
-        if (err) reject(err);
-        resolve(res.affectedRows);
-      });
-    });
-  },
 };
